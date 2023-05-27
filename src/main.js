f82651fdc7cc0ea957f917bdd25badec6627a602
shizudio <shinafxt2000@gmail.com>
2023-05-27 17:21:36 +0800

    modify according to eslint

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import { arbitrum, mainnet, polygon } from '@wagmi/core/chains'
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { Web3Modal } from '@web3modal/html'
 
-
 const chains = [arbitrum, mainnet, polygon]
 const projectId = '7ffae9cc3bdc1f84297b73068118bd83'
 
@@ -11,7 +10,7 @@ const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
 const wagmiConfig = createConfig({
   autoConnect: true,
   connectors: w3mConnectors({ projectId, version: 1, chains }),
-  publicClient
+  publicClient,
 })
 const ethereumClient = new EthereumClient(wagmiConfig, chains)
 const web3modal = new Web3Modal({ projectId }, ethereumClient)
