eb1bfc337baedf68724befea5806638b67b6d10a
shizudio <shinafxt2000@gmail.com>
2023-05-27 14:39:50 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/react'
-import { configureChains, createConfig, WagmiConfig } from 'wagmi'
-import { arbitrum, mainnet, polygon } from 'wagmi/chains'
+import { Web3Modal } from '@web3modal/html'
+import { configureChains, createConfig } from '@wagmi/core'
+import { arbitrum, mainnet, polygon } from '@wagmi/core/chains'
 
 const chains = [arbitrum, mainnet, polygon]
 const projectId = '7ffae9cc3bdc1f84297b73068118bd83'
@@ -13,15 +13,4 @@ const wagmiConfig = createConfig({
   publicClient
 })
 const ethereumClient = new EthereumClient(wagmiConfig, chains)
-
-function App() {
-  return (
-    <>
-      <WagmiConfig config={wagmiConfig}>
-        <HomePage />
-      </WagmiConfig>
-
-      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
-    </>
-  )
-}
+const web3modal = new Web3Modal({ projectId }, ethereumClient)
